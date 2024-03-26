5d2fba3644281a6b0d6a70d395256717caa3f267
JSHGRAM\wjdtk <wjdtkdgns234@naver.com>
2024-03-26 18:41:21 +0900

    [fix] IntroScreen 안의 Lazy 컴포넌트 중첩 오류 수정

--- feature/intro/src/main/kotlin/com/unifest/android/feature/intro/IntroScreen.kt
+++ feature/intro/src/main/kotlin/com/unifest/android/feature/intro/IntroScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -21,6 +24,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
@@ -62,41 +66,29 @@ internal fun IntroRoute(
 fun IntroScreen() {
     val selectedSchools = remember { mutableStateListOf<School>() }
     var searchText by remember { mutableStateOf("") }
-    val initialSelectedSchoolsSnapshot = remember { selectedSchools.toList() }
-    val hasChanges = remember { mutableStateOf(false) }
-    val currentSnapshot = selectedSchools.toList()
-    LaunchedEffect(currentSnapshot) {
-        hasChanges.value = initialSelectedSchoolsSnapshot != currentSnapshot
-    }
     //todo: 유저가 관심 축제 저장하고 가져오는 로직 추가
-    Column {
-        InformationText()
-        SearchBar(
-            searchText = searchText, onValueChange = { searchText = it },
-            onSearch = { query ->
-
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            InformationText()
+        }
+        item {
+            SearchBar(searchText = searchText, onValueChange = { searchText = it }) { query ->
                 println("검색: $query")
-            },
-        )
-        SelectedSchoolsGrid(selectedSchools = selectedSchools)
-        AllSchoolsTabView(
-            onSchoolSelected = { school ->
-                if (!selectedSchools.any { it.schoolName == school.schoolName }) {
-                    selectedSchools.add(school)
-                }
-            },
-        )
-        if (hasChanges.value) {
-            Button(
-                onClick = {
-                    // 변경사항을 처리하고 이전 화면으로 돌아가는 로직 추가
-//                    navigator?
-                },
-                modifier = Modifier.padding(16.dp),
-            ) {
-                Text("완료")
             }
         }
+        item {
+            SelectedSchoolsGrid(selectedSchools)
+        }
+        item {
+            AllSchoolsTabView(
+                onSchoolSelected = { school ->
+                    if (!selectedSchools.any { it.schoolName == school.schoolName }) {
+                        selectedSchools.add(school)
+                    }
+                },
+            )
+        }
+        //todo:버튼 추가
     }
 }
 
@@ -143,7 +135,13 @@ fun SearchBar(
             text = it
             onValueChange(it)
         },
-        placeholder = { Text("학교를 검색해보세요", color = Color.Gray, fontSize = 13.sp) },
+        placeholder = {
+            Text(
+                "학교를 검색해보세요",
+                color = Color.Gray,
+                fontSize = 13.sp,
+            )
+        },
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 12.dp, bottom = 16.dp, start = 20.dp, end = 20.dp),
@@ -158,7 +156,7 @@ fun SearchBar(
                 imageVector = Icons.Default.Search,
                 contentDescription = "Search Icon",
                 tint = Color.Gray,
-                modifier = Modifier.size(30.dp)
+                modifier = Modifier.size(30.dp),
             )
         },
     )
@@ -183,64 +181,66 @@ fun SelectedSchoolsGrid(selectedSchools: MutableList<School>) {
                 modifier = Modifier.align(Alignment.CenterVertically),
             ) {
                 Text(
-                    text = "모두 선택 해제",
+                    text="모두 선택 해제",
                     color = Color.Gray,
                     textDecoration = TextDecoration.Underline,
                 )
             }
         }
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            contentPadding = PaddingValues(12.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            items(selectedSchools) { school ->
-                SchoolItem(school = school, onSchoolSelected = { selectedSchools.remove(school) })
+
+        selectedSchools.chunked(3).forEach { rowSchools ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                rowSchools.forEach { school ->
+                    SchoolItem(school = school, onSchoolSelected = { selectedSchools.remove(it) })
+                }
+
+                repeat(3 - rowSchools.size) {
+                    Spacer(modifier = Modifier.weight(1f))
+                }
             }
         }
     }
 }
 
+
 @Composable
-fun SchoolItem(
-    school: School,
-    onSchoolSelected: (School) -> Unit,
-) {
+fun SchoolItem(school: School, onSchoolSelected: (School) -> Unit) {
     Card(
         shape = RoundedCornerShape(16.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White, contentColor = Color.Black),
         border = BorderStroke(1.dp, Color.LightGray),
         modifier = Modifier
-            .fillMaxWidth()
-            .clickable { onSchoolSelected(school) },
+            .clickable { onSchoolSelected(school) }
+            .padding(4.dp),
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
-            modifier = Modifier.padding(24.dp),
+            modifier = Modifier.padding(16.dp),
         ) {
-
             Image(
-                painter = painterResource(id = R.drawable.ic_waiting),
-                //todo: coil로 학교 마크추가
-                contentDescription = "School Mark",
-                modifier = Modifier.size(35.dp)
+                painter = painterResource(id = R.drawable.ic_map),
+                contentDescription = null,
+                modifier = Modifier.size(50.dp),
             )
-            Text(school.schoolName)
-            Text(school.festivalName, fontWeight = FontWeight.Bold)
-            Text(school.festivalDate, color = Color.Gray)
-
+            //todo:coil로 이미지 넣기
+            Text(school.schoolName, fontWeight = FontWeight.Bold)
+            Text(school.festivalName)
+            Text(school.festivalDate)
         }
     }
 }
-
 
 @Composable
 fun AllSchoolsTabView(onSchoolSelected: (School) -> Unit) {
     val tabTitles = listOf("전체", "서울", "경기/인천", "강원", "대전/충청", "광주/전라", "부산/대구", "경상도")
     var selectedTabIndex by remember { mutableIntStateOf(0) }
-    val selectedColor = Color(0xFFF5687E) // 선택된 탭의 글자 색
+    val selectedColor = Color(0xFFF5687E)
     val unselectedColor = Color.Black
 
     ScrollableTabRow(
@@ -279,7 +279,6 @@ fun AllSchoolsTabView(onSchoolSelected: (School) -> Unit) {
     // 이후 items(schools) { school -> } 에 filteredSchools를 넣어줄예정.
 
     Column(modifier = Modifier.padding(top = 8.dp)) {
-        // 학교의 수를 표시하는 Text 컴포저블 추가
         Text(
             text = "총 ${schools.size}개",
             modifier = Modifier
@@ -287,14 +286,29 @@ fun AllSchoolsTabView(onSchoolSelected: (School) -> Unit) {
                 .align(Alignment.Start),
         )
 
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            contentPadding = PaddingValues(12.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            items(schools) { school ->
-                SchoolItem(school = school, onSchoolSelected = onSchoolSelected)
+        val rows = (schools.size + 2) / 3
+        Column {
+            for (row in 0 until rows) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    for (col in 0 until 3) {
+                        val index = row * 3 + col
+                        if (index < schools.size) {
+                            val school = schools[index]
+                            SchoolItem(school = school, onSchoolSelected = onSchoolSelected)
+                        } else {
+                            Spacer(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(8.dp),
+                            )
+                        }
+                    }
+                }
             }
         }
     }
