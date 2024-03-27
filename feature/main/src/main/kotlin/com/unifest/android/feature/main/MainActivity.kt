ad23f7feebc4262ab4416cbfaf4cede37119b73f
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-27 17:37:35 +0900

    [style] MainActivity 컴포넌트 색상 수정 및 WindowInsets 비활성화
    바텀시트 및 배경 색상 White 로 고정

--- feature/main/src/main/kotlin/com/unifest/android/feature/main/MainActivity.kt
+++ feature/main/src/main/kotlin/com/unifest/android/feature/main/MainActivity.kt
@@ -3,14 +3,12 @@ package com.unifest.android.feature.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.unifest.android.core.designsystem.theme.UnifestTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             val navigator: MainNavController = rememberMainNavController()
