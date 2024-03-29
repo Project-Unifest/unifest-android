d7afe6b6e52670dcdf95ef6e7768ea43de4a300e
DESKTOP-E152GOJ\aaa <wjdtkdgns234@naver.com>
2024-03-29 15:15:49 +0900

    [chore] 주석 추가

--- feature/intro/src/main/kotlin/com/unifest/android/feature/intro/IntroScreen.kt
+++ feature/intro/src/main/kotlin/com/unifest/android/feature/intro/IntroScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -91,8 +92,7 @@ fun IntroScreen(navigateToMain: () -> Unit) {
                     }
                 })
             }
-        }
-
+        }//추가 완료 버튼을 위해 Box로
 
         Button(
             onClick = { navigateToMain() },
@@ -140,6 +140,7 @@ fun InformationText() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
+    //학교 검색
     searchText: String,
     onValueChange: (String) -> Unit,
     onSearch: (String) -> Unit,
@@ -178,8 +179,10 @@ fun SearchBar(
     )
 }
 
+
 @Composable
 fun SelectedSchoolsGrid(selectedSchools: MutableList<School>) {
+    //나의 관심 축제
     Column {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -226,6 +229,7 @@ fun SelectedSchoolsGrid(selectedSchools: MutableList<School>) {
 
 @Composable
 fun SchoolItem(school: School, onSchoolSelected: (School) -> Unit) {
+    //그리드에 들어갈 각 학교별 아이템
     Card(
         shape = RoundedCornerShape(16.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White, contentColor = Color.Black),
@@ -233,6 +237,7 @@ fun SchoolItem(school: School, onSchoolSelected: (School) -> Unit) {
         modifier = Modifier
             .clickable { onSchoolSelected(school) }
             .padding(4.dp),
+
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -255,12 +260,14 @@ fun SchoolItem(school: School, onSchoolSelected: (School) -> Unit) {
 
 @Composable
 fun AllSchoolsTabView(onSchoolSelected: (School) -> Unit) {
+    //전체 학교 그리드뷰
     val tabTitles = listOf("전체", "서울", "경기/인천", "강원", "대전/충청", "광주/전라", "부산/대구", "경상도")
     var selectedTabIndex by remember { mutableIntStateOf(0) }
     val selectedColor = Color(0xFFF5687E)
     val unselectedColor = Color.Black
 
     ScrollableTabRow(
+        //지역 탭
         selectedTabIndex = selectedTabIndex,
         indicator = {},
         edgePadding = 0.dp,
@@ -281,10 +288,6 @@ fun AllSchoolsTabView(onSchoolSelected: (School) -> Unit) {
         }
     }
 
-    // todo:선택된 지역탭을 기반으로 필터링된 대학교 목록을 가져오게 구현
-    // val filteredSchools = schools.filter { }
-    // 이후 items(schools) { school -> } 에 filteredSchools를 넣어줄예정.
-
     Column(modifier = Modifier.padding(top = 8.dp)) {
         Text(
             text = "총 ${schools.size}개",
@@ -292,27 +295,23 @@ fun AllSchoolsTabView(onSchoolSelected: (School) -> Unit) {
                 .padding(start = 20.dp)
                 .align(Alignment.Start),
         )
+        //총 학교수
 
         val rows = (schools.size + 2) / 3
+        //학교 수에 따른 행 수
+
         Column {
             for (row in 0 until rows) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
+                        .padding(horizontal = 8.dp),
                 ) {
                     for (col in 0 until 3) {
                         val index = row * 3 + col
                         if (index < schools.size) {
                             val school = schools[index]
                             SchoolItem(school = school, onSchoolSelected = onSchoolSelected)
-                        } else {
-                            Spacer(
-                                modifier = Modifier
-                                    .weight(1f)
-                                    .padding(8.dp),
-                            )
                         }
                     }
                 }
@@ -323,6 +322,7 @@ fun AllSchoolsTabView(onSchoolSelected: (School) -> Unit) {
 
 
 
+
 // 임시 데이터
 val schools = listOf(
     School("school_image_url_1", "서울대학교", "설대축제", "05.06-05.08"),
@@ -330,5 +330,11 @@ val schools = listOf(
     School("school_image_url_3", "고려대학교", "고대축제", "05.06-05.08"),
     School("school_image_url_4", "건국대학교", "녹색지대", "05.06-05.08"),
     School("school_image_url_5", "성균관대", "성대축제", "05.06-05.08"),
-    School("school_image_url_5", "한양대", "한양대축제", "05.06-05.08")
+
 )
+
+@Preview
+@Composable
+fun PreviewIntroScreen() {
+    IntroScreen(navigateToMain = {})
+}
