e0bcdfa89aaf8cb64659012be9c4b5b5fffc5628
DESKTOP-E152GOJ\aaa <wjdtkdgns234@naver.com>
2024-03-29 14:46:53 +0900

    [add] SchoolEntity 추가

---
+++ core/domain/src/main/kotlin/com/unifest/android/core/domain/entity/SchoolEntity.kt
@@ -0,0 +1,9 @@
+package com.unifest.android.core.domain.entity
+
+
+data class School(
+    val image: String,
+    val schoolName: String,
+    val festivalName: String,
+    val festivalDate: String
+)
