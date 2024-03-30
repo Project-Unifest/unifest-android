6a4892ffc2c745609ce109ea323cea229b9546b7
DESKTOP-E152GOJ\aaa <pathfinder8851@gmail.com>
2024-03-30 18:43:42 +0900

    [setting] 네이버 지도로 변경 환경설정

--- feature/map/src/main/kotlin/com/unifest/android/feature/map/MapScreen.kt
+++ feature/map/src/main/kotlin/com/unifest/android/feature/map/MapScreen.kt
@@ -13,14 +13,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+//import com.google.android.gms.maps.model.BitmapDescriptorFactory
+//import com.google.android.gms.maps.model.CameraPosition
+//import com.google.android.gms.maps.model.LatLng
+//import com.google.maps.android.compose.GoogleMap
+//import com.google.maps.android.compose.MapUiSettings
+//import com.google.maps.android.compose.Marker
+//import com.google.maps.android.compose.MarkerState
+//import com.google.maps.android.compose.rememberCameraPositionState
+import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.Marker
+import com.naver.maps.map.compose.MarkerState
+import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.compose.rememberCameraPositionState
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
 import com.unifest.android.core.designsystem.theme.UnifestTheme
 import com.unifest.android.core.ui.DevicePreview
 import com.unifest.android.feature.map.viewmodel.MapUiState
@@ -39,14 +48,15 @@ internal fun MapRoute(
     )
 }
 
+@OptIn(ExperimentalNaverMapApi::class)
 @Composable
 internal fun MapScreen(
     padding: PaddingValues,
     uiState: MapUiState,
 ) {
-    val uiSettings = remember {
-        MapUiSettings(zoomControlsEnabled = false)
-    }
+//    val uiSettings = remember {
+//        MapUiSettings(zoomControlsEnabled = false)
+//    }
 
     Column(
         modifier = Modifier
@@ -56,32 +66,55 @@ internal fun MapScreen(
         verticalArrangement = Arrangement.Center,
     ) {
         val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(LatLng(37.540470588662664, 127.0765263757882), 16.5f)
+            position = CameraPosition(LatLng(37.540470588662664, 127.0765263757882), 14.0)
         }
 
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState,
-            properties = uiState.properties,
-            uiSettings = uiSettings,
-            onMapClick = {},
-        ) {
-            uiState.boothSpots.forEach { spot ->
+        NaverMap(cameraPositionState = cameraPositionState) {
+            data class BoothSpot(val latitude: Double, val longitude: Double)
+
+            val boothSpots = listOf(
+                BoothSpot(37.54053013863604, 127.07505652524804),
+                BoothSpot(37.54111712868565, 127.07839319326257),
+                BoothSpot(37.5414744247141, 127.07779237844323),
+                BoothSpot(37.54224856023523, 127.07605430700158),
+                BoothSpot(37.54003672313541, 127.07653710462426),
+                BoothSpot(37.53998567996623, 37.53998567996623),
+                BoothSpot(37.54152546686414, 127.07353303052759),
+                BoothSpot(37.54047909580466, 127.07398364164209),
+            )
+
+            // boothSpots 리스트에서 마커 생성
+            boothSpots.forEach { spot ->
                 Marker(
-                    state = MarkerState(
-                        position = LatLng(spot.lat, spot.lng),
-                    ),
-                    title = "Booth spot (${spot.lat}, ${spot.lng})",
-                    snippet = "click",
-                    onClick = { true },
-                    icon = BitmapDescriptorFactory.defaultMarker(
-                        BitmapDescriptorFactory.HUE_GREEN,
-                    ),
+                    state = MarkerState(position = LatLng(spot.latitude, spot.longitude)),
+                    icon = OverlayImage.fromResource(com.unifest.android.core.designsystem.R.drawable.ic_general)
                 )
             }
         }
     }
 }
+
+//        GoogleMap(
+//            modifier = Modifier.fillMaxSize(),
+//            cameraPositionState = cameraPositionState,
+//            properties = uiState.properties,
+//            uiSettings = uiSettings,
+//            onMapClick = {},
+//        ) {
+//            uiState.boothSpots.forEach { spot ->
+//                Marker(
+//                    state = MarkerState(
+//                        position = LatLng(spot.lat, spot.lng),
+//                    ),
+//                    title = "Booth spot (${spot.lat}, ${spot.lng})",
+//                    snippet = "click",
+//                    onClick = { true },
+//                    icon = BitmapDescriptorFactory.defaultMarker(
+//                        BitmapDescriptorFactory.HUE_GREEN,
+//                    ),
+//                )
+//            }
+//        }
 
 @DevicePreview
 @Composable
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = uri("https://devrepo.kakao.com/nexus/content/groups/public/") }
+        maven("https://repository.map.naver.com/archive/maven")
     }
 }
 
