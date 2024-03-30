0116e0262528d99cd4ace045166e1c75f93cd663
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-30 11:42:17 +0900

    [feat] WindowInsets 재 활성화
    calculateBottomPadding() 과 같은 modifier 적용을 위함

--- feature/main/src/main/kotlin/com/unifest/android/feature/main/MainActivity.kt
+++ feature/main/src/main/kotlin/com/unifest/android/feature/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.unifest.android.feature.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.unifest.android.core.designsystem.theme.UnifestTheme
 import com.unifest.feature.navigator.IntroNavigator
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var introNavigator: IntroNavigator
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             val navigator: MainNavController = rememberMainNavController()
