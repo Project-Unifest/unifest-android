8677565d890118cc7dd05231b188251e13e09591
Jeong Sang Hoon <wjdtkdgns234@naver.com>
2024-04-02 18:11:39 +0900

    [feat] UnifestOutLinedButton컴포넌트 추가

---
+++ core/designsystem/src/main/kotlin/com/unifest/android/core/designsystem/component/OutLinedButton.kt
@@ -0,0 +1,47 @@
+package com.unifest.android.core.designsystem.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.unifest.android.core.designsystem.ComponentPreview
+
+@Composable
+fun UnifestOutlinedButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    borderColor: Color = Color(0xFFf5678E),
+    contentColor: Color = Color.White,
+    enabled: Boolean = true,
+    content: @Composable RowScope.() -> Unit,
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = contentColor
+        ),
+        border = BorderStroke(1.dp, borderColor),
+        contentPadding = PaddingValues(horizontal = 20.dp, vertical = 12.dp),
+        content = content,
+    )
+}
+
+@ComponentPreview
+@Composable
+fun UnifestOutlinedButtonPreview() {
+    UnifestOutlinedButton(
+        onClick = {},
+    ) {
+        Text("Outlined Button")
+    }
+}
