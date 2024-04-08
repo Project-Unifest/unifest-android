1173a636c8693c761ff2cda15ac29dc9664082f3
Jeong Sang Hoon <wjdtkdgns234@naver.com>
2024-04-09 02:36:40 +0900

    [chore] style check success

--- feature/booth/src/main/kotlin/com/unifest/android/feature/booth/BoothLocationScreen.kt
+++ feature/booth/src/main/kotlin/com/unifest/android/feature/booth/BoothLocationScreen.kt
@@ -1,7 +1,6 @@
 package com.unifest.android.feature.booth
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,7 +13,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
