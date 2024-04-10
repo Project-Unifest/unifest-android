105dfdf70e761f23feb4716731d91690abf8d6fd
Jeong Sang Hoon <wjdtkdgns234@naver.com>
2024-04-11 02:01:23 +0900

    [feat] Menu Ui 작성
    (cherry picked from commit cbacf80863164c9ed3fed3497d662bbc9474ca29)
    (cherry picked from commit d1602d76fed02f005a2c42124b0f43fbd00e4905)

--- feature/menu/src/main/kotlin/com/unifest/android/feature/menu/MenuScreen.kt
+++ feature/menu/src/main/kotlin/com/unifest/android/feature/menu/MenuScreen.kt
@@ -1,28 +1,333 @@
 package com.unifest.android.feature.menu
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import com.unifest.android.core.designsystem.theme.UnifestTheme
+import com.unifest.android.core.domain.entity.MenuEntity
+import com.unifest.android.core.ui.DevicePreview
+import kotlinx.collections.immutable.persistentListOf
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Help
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.CheckBoxOutlineBlank
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.unifest.android.core.designsystem.theme.UnifestTheme
-import com.unifest.android.core.ui.DevicePreview
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.unifest.android.core.designsystem.R
+import com.unifest.android.core.designsystem.component.TopAppBarNavigationType
+import com.unifest.android.core.designsystem.component.UnifestTopAppBar
+import com.unifest.android.core.designsystem.theme.Content2
+import com.unifest.android.core.designsystem.theme.Content4
+import com.unifest.android.core.designsystem.theme.Content6
+import com.unifest.android.core.designsystem.theme.Content7
+import com.unifest.android.core.designsystem.theme.Content8
+import com.unifest.android.core.designsystem.theme.MenuTitle
+import com.unifest.android.core.designsystem.theme.Title2
+import com.unifest.android.core.designsystem.theme.Title3
+import com.unifest.android.core.designsystem.theme.Title5
+import com.unifest.android.core.domain.entity.BoothDetailEntity
+import com.unifest.android.core.domain.entity.Festival
+import com.unifest.android.feature.menu.viewmodel.MenuUiState
+import com.unifest.android.feature.menu.viewmodel.MenuViewModel
 
 @Composable
-internal fun MenuRoute() {
-    MenuScreen()
+internal fun MenuRoute(
+    viewModel: MenuViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    MenuScreen(
+        uiState = uiState,
+    )
+
 }
 
 @Composable
-fun MenuScreen() {
+fun MenuScreen(
+    uiState: MenuUiState,
+) {
+
+    Box {
+        Column {
+            UnifestTopAppBar(
+                navigationType = TopAppBarNavigationType.None,
+                title = "메뉴",
+                elevation = 8.dp,
+                modifier = Modifier.background(Color.White, shape = RoundedCornerShape(bottomEnd = 20.dp, bottomStart = 20.dp)),
+            )
+            LazyColumn {
+                item { Spacer(modifier = Modifier.height(5.dp)) }
+                item {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp),
+                    ) {
+                        Text(text = "나의 관심학교", style = Title3)
+                        Text(text = "추가하기>", style = Content7, color = Color(0xFF545454))
+                    }
+                }
+                item {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(4),
+                        modifier = Modifier
+
+                            .height(
+                                when {
+                                    uiState.festivals.isEmpty() -> 0.dp
+                                    else -> {
+                                        val rows = ((uiState.festivals.size - 1) / 4 + 1) * 130
+                                        rows.dp
+                                    }
+                                },
+                            ),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                    ) {
+                        items(uiState.festivals.size) { index ->
+                            val school = uiState.festivals[index]
+                            SchoolItem(
+                                festival = school,
+                            )
+                        }
+                    }
+                }
+                item {
+                    VerticalDivider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(8.dp)
+                            .background(Color(0xFFF1F3F7)),
+                    )
+                }
+                item {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 20.dp, end = 20.dp, top = 20.dp),
+                    ) {
+                        Text(text = "관심부스", style = Title3)
+                        Text(text = "더보기>", style = Content7, color = Color(0xFF545454))
+                    }
+                }
+
+                itemsIndexed(uiState.interestedBooths) { index, booth ->
+                    Column {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        IncomingFestivalCard(booth)
+                    }
+                    if (index < uiState.interestedBooths.size - 1) {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        HorizontalDivider(
+                            color = Color(0xFFDFDFDF),
+                            modifier = Modifier.padding(horizontal = 20.dp),
+                            thickness = 1.dp,
+                        )
+                    }
+                }
+                item { Spacer(modifier = Modifier.height(16.dp)) }
+                item {
+                    VerticalDivider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(8.dp)
+                            .background(Color(0xFFF1F3F7)),
+                    )
+                }
+                item {
+                    MenuItem(
+                        ImageVector.vectorResource(R.drawable.ic_inquiry),
+                        title = "이용 문의",
+                        onClick = { /* 구현 */ },
+                    )
+                }
+                item {
+                    VerticalDivider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(Color(0xFFE0E0E0)),
+                    )
+                }
+                item {
+                    MenuItem(
+                        ImageVector.vectorResource(R.drawable.ic_admin_mode),
+                        title = "운영자 모드 진입",
+                        onClick = { /* 구현 */ },
+                    )
+                }
+                item {
+                    VerticalDivider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(Color(0xFFE0E0E0)),
+                    )
+                }
+                item {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 13.dp),
+                    ) {
+                        Text("UniFest ${uiState.appVersion}", textAlign = TextAlign.Center, color = Color(0xFFC5C5C5))
+                    }
+                }
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun MenuItem(icon: ImageVector, title: String, onClick: () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(25.dp),
+    ) {
+        Icon(icon, contentDescription = null, tint = Color(0xFF7A7A7C))
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(title, style = Content8)
+    }
+}
+
+@Composable
+fun SchoolItem(
+    festival: Festival,
+) {
     Column(
-        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
     ) {
-        Text("Menu Screen")
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .shadow(
+                    elevation = 4.dp,
+                    shape = CircleShape,
+                    clip = false,
+                )
+                .background(Color.White, CircleShape)
+                .padding(5.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_waiting),
+                contentDescription = null,
+                modifier = Modifier.size(50.dp),
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = festival.schoolName,
+            color = Color(0xFF545454),
+            style = Content6,
+        )
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            text = festival.festivalName,
+            color = Color.Black,
+            style = MenuTitle,
+        )
+    }
+
+}
+
+@Composable
+fun IncomingFestivalCard(booth: BoothDetailEntity) {
+    val isBookmarked = true
+    val bookMarkColor = if (isBookmarked) Color(0xFFF5687E) else Color(0xFF4B4B4B)
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .fillMaxSize(),
+
+        ) {
+        Icon(
+            imageVector = Icons.Outlined.CheckBoxOutlineBlank,
+            contentDescription = "Example Image Icon",
+            modifier = Modifier.size(86.dp),
+        )
+        Spacer(modifier = Modifier.width(14.dp))
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+        ) {
+            Text(
+                text = booth.name,
+                style = Title2,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = booth.description,
+                style = Title5,
+                color = Color(0xFF545454),
+            )
+            Spacer(modifier = Modifier.height(13.dp))//todo:23dp 너무 멀음
+            Row {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_location_green),
+                    contentDescription = "Location Icon",
+                    tint = Color.Unspecified,
+                )
+                Spacer(modifier = Modifier.width(3.dp))
+                Text(
+                    text = booth.location,
+                    style = Title5,
+                    color = Color(0xFF545454),
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                )
+            }
+        }
+        Icon(
+            imageVector = ImageVector.vectorResource(if (isBookmarked) R.drawable.ic_bookmarked else R.drawable.ic_bookmark),
+            contentDescription = if (isBookmarked) "북마크됨" else "북마크하기",
+            tint = bookMarkColor,
+        )
     }
 }
 
@@ -30,6 +335,91 @@ fun MenuScreen() {
 @Composable
 fun MenuScreenPreview() {
     UnifestTheme {
-        MenuScreen()
+        MenuScreen(
+            uiState = MenuUiState(
+                festivals = persistentListOf(
+                    Festival(
+                        schoolName = "건국대",
+                        festivalName = "녹색지대",
+                        festivalDate = "2021.11.11",
+                        imgUrl = "",
+                    ),
+                    Festival(
+                        schoolName = "건국대",
+                        festivalName = "녹색지대",
+                        festivalDate = "2021.11.11",
+                        imgUrl = "",
+                    ),
+                    Festival(
+                        schoolName = "건국대",
+                        festivalName = "녹색지대",
+                        festivalDate = "2021.11.11",
+                        imgUrl = "",
+                    ),
+                    Festival(
+                        schoolName = "건국대",
+                        festivalName = "녹색지대",
+                        festivalDate = "2021.11.11",
+                        imgUrl = "",
+                    ),
+                ),
+                interestedBooths = persistentListOf(
+                    BoothDetailEntity(
+                        id = 1,
+                        name = "부스 이름",
+                        category = "음식",
+                        description = "부스 설명",
+                        warning = "주의사항",
+                        location = "부스 위치",
+                        latitude = 0.0f,
+                        longitude = 0.0f,
+                        menus = listOf(
+                            MenuEntity(
+                                id = 1,
+                                name = "메뉴 이름",
+                                price = 1000,
+                                imgUrl = "",
+                            ),
+                        ),
+                    ),
+                    BoothDetailEntity(
+                        id = 2,
+                        name = "부스 이름",
+                        category = "음식",
+                        description = "부스 설명",
+                        warning = "주의사항",
+                        location = "부스 위치",
+                        latitude = 0.0f,
+                        longitude = 0.0f,
+                        menus = listOf(
+                            MenuEntity(
+                                id = 1,
+                                name = "메뉴 이름",
+                                price = 1000,
+                                imgUrl = "",
+                            ),
+                        ),
+                    ),
+//                    BoothDetailEntity(
+//                        id = 3,
+//                        name = "부스 이름",
+//                        category = "음식",
+//                        description = "부스 설명",
+//                        warning = "주의사항",
+//                        location = "부스 위치",
+//                        latitude = 0.0f,
+//                        longitude = 0.0f,
+//                        menus = listOf(
+//                            MenuEntity(
+//                                id = 1,
+//                                name = "메뉴 이름",
+//                                price = 1000,
+//                                imgUrl = "",
+//                            ),
+//                        ),
+//                    ),
+                ),
+            ),
+        )
     }
 }
