2688bc42e4ed480c93b0ed6b6ee71d99060a1aac
Jeong Sang Hoon <wjdtkdgns234@naver.com>
2024-04-11 01:49:48 +0900

    [feat] Menu Screen 앱 버젼 얻어오는 로직 추가
    (cherry picked from commit 11d57deb61b4bdf840358ca2aeedecfa7959052d)
    (cherry picked from commit 779f9605840ea25de74b308569ec78ad459b0d76)

---
+++ feature/menu/src/main/kotlin/com/unifest/android/feature/menu/viewmodel/MenuUiState.kt
@@ -0,0 +1,12 @@
+package com.unifest.android.feature.menu.viewmodel
+
+import com.unifest.android.core.domain.entity.BoothDetailEntity
+import com.unifest.android.core.domain.entity.Festival
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+data class MenuUiState(
+    val festivals: ImmutableList<Festival> = persistentListOf(),
+    val interestedBooths: ImmutableList<BoothDetailEntity> = persistentListOf(),
+    val appVersion: String = ""
+)
--- feature/menu/src/main/kotlin/com/unifest/android/feature/menu/viewmodel/MenuViewModel.kt
+++ feature/menu/src/main/kotlin/com/unifest/android/feature/menu/viewmodel/MenuViewModel.kt
@@ -1,8 +1,81 @@
 package com.unifest.android.feature.menu.viewmodel
 
+import android.app.Application
+import android.content.pm.PackageManager
 import androidx.lifecycle.ViewModel
+import com.unifest.android.core.domain.entity.BoothDetailEntity
+import com.unifest.android.core.domain.entity.MenuEntity
+import com.unifest.android.core.domain.entity.Festival
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class MenuViewModel @Inject constructor() : ViewModel()
+class MenuViewModel @Inject constructor(private val application: Application) : ViewModel() {
+    private val _uiState = MutableStateFlow(MenuUiState())
+    val uiState: StateFlow<MenuUiState> = _uiState.asStateFlow()
+
+    init {
+
+        val appVersion = try {
+            application.packageManager.getPackageInfo(application.packageName, 0).versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            "Unknown"
+        }
+
+        _uiState.update {currentState ->
+            currentState.copy(
+                appVersion = appVersion,
+                // 임시 데이터
+                festivals = persistentListOf(
+                    Festival("school_image_url_1", "서울대학교", "설대축제", "05.06-05.08"),
+                    Festival("school_image_url_2", "연세대학교", "연대축제", "05.06-05.08"),
+                    Festival("school_image_url_3", "고려대학교", "고대축제", "05.06-05.08"),
+                    Festival("school_image_url_4", "건국대학교", "녹색지대", "05.06-05.08"),
+                    Festival("school_image_url_5", "성균관대", "성대축제", "05.06-05.08"),
+                ),
+
+                interestedBooths = persistentListOf(
+                    BoothDetailEntity(
+                        id = 1,
+                        name = "부스1",
+                        category = "카페",
+                        description = "부스1 설명",
+                        warning = "부스1 주의사항",
+                        location = "부스1 위치",
+                        latitude = 37.5665f,
+                        longitude = 126.9780f,
+                        menus = listOf(),
+                    ),
+                    BoothDetailEntity(
+                        id = 2,
+                        name = "부스2",
+                        category = "카페",
+                        description = "부스2 설명",
+                        warning = "부스2 주의사항",
+                        location = "부스2 위치",
+                        latitude = 37.5665f,
+                        longitude = 126.9780f,
+                        menus = listOf(),
+                    ),
+                    BoothDetailEntity(
+                        id = 3,
+                        name = "부스3",
+                        category = "카페",
+                        description = "부스3 설명",
+                        warning = "부스3 주의사항",
+                        location = "부스3 위치",
+                        latitude = 37.5665f,
+                        longitude = 126.9780f,
+                        menus = listOf(),
+                    ),
+                ),
+            )
+        }
+    }
+
+}
