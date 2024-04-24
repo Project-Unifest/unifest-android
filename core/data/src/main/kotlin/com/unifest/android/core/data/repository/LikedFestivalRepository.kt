1c4454e6987026aa10c8b0cff12c3b240b588004
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-04-24 22:49:52 +0900

    [refactor] festivalRepository 와 LikedRepository 분리

--- core/data/src/main/kotlin/com/unifest/android/core/data/repository/LikedFestivalRepository.kt
+++ core/data/src/main/kotlin/com/unifest/android/core/data/repository/LikedFestivalRepository.kt
@@ -1,6 +1,14 @@
 package com.unifest.android.core.data.repository
 
+import com.unifest.android.core.model.FestivalModel
+import com.unifest.android.core.model.FestivalTodayModel
+import kotlinx.coroutines.flow.Flow
+
 interface LikedFestivalRepository {
+    fun getLikedFestivals(): Flow<List<FestivalModel>>
+    suspend fun insertLikedFestivalAtHome(festival: FestivalTodayModel)
+    suspend fun insertLikedFestivalAtSearch(festival: FestivalModel)
+    suspend fun deleteLikedFestival(festival: FestivalModel)
     suspend fun getRecentLikedFestival(): String
     suspend fun setRecentLikedFestival(schoolName: String)
 }
