9e196176a4f50df4093678a91d4a8048376c883e
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-04-27 07:22:29 +0900

    [feat] 클러스터링 TedClustering -> NaverMapClustering 으로 Migration

--- feature/map/src/main/kotlin/com/unifest/android/feature/map/model/BoothDetailMapModel.kt
+++ feature/map/src/main/kotlin/com/unifest/android/feature/map/model/BoothDetailMapModel.kt
@@ -1,9 +1,9 @@
 package com.unifest.android.feature.map.model
 
 import android.os.Parcelable
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.clustering.ClusteringKey
 import kotlinx.parcelize.Parcelize
-import ted.gun0912.clustering.clustering.TedClusterItem
-import ted.gun0912.clustering.geometry.TedLatLng
 
 @Parcelize
 data class BoothDetailMapModel(
@@ -16,11 +16,28 @@ data class BoothDetailMapModel(
     val latitude: Double = 0.toDouble(),
     val longitude: Double = 0.toDouble(),
     val menus: List<MenuMapModel> = emptyList(),
-) : Parcelable, TedClusterItem {
-    override fun getTedLatLng(): TedLatLng {
-        return TedLatLng(latitude, longitude)
+) : Parcelable, ClusteringKey {
+    override fun getPosition(): LatLng {
+        return LatLng(latitude, longitude)
     }
 }
+
+//@Parcelize
+//data class BoothDetailMapModel(
+//    val id: Long = 0L,
+//    val name: String = "",
+//    val category: String = "",
+//    val description: String = "",
+//    val warning: String = "",
+//    val location: String = "",
+//    val latitude: Double = 0.toDouble(),
+//    val longitude: Double = 0.toDouble(),
+//    val menus: List<MenuMapModel> = emptyList(),
+//) : Parcelable, TedClusterItem {
+//    override fun getTedLatLng(): TedLatLng {
+//        return TedLatLng(latitude, longitude)
+//    }
+//}
 
 @Parcelize
 data class MenuMapModel(
