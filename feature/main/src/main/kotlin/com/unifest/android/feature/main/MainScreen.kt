9c701620b052bdfc4e1b0d50688e353f579f713e
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-04-26 16:29:11 +0900

    [chore] 중복된 코루틴 빌더 함수 제거

--- feature/main/src/main/kotlin/com/unifest/android/feature/main/MainScreen.kt
+++ feature/main/src/main/kotlin/com/unifest/android/feature/main/MainScreen.kt
@@ -55,22 +55,20 @@ private const val SnackBarDuration = 1000L
 internal fun MainScreen(
     navigator: MainNavController = rememberMainNavController(),
 ) {
-    val snackBarstate = remember { SnackbarHostState() }
+    val snackBarState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
 
     val onShowSnackBar: (message: UiText) -> Unit = { message ->
         scope.launch {
-            scope.launch {
-                val job = launch {
-                    snackBarstate.showSnackbar(
-                        message = message.asString(context),
-                        duration = SnackbarDuration.Short,
-                    )
-                }
-                delay(SnackBarDuration)
-                job.cancel()
+            val job = launch {
+                snackBarState.showSnackbar(
+                    message = message.asString(context),
+                    duration = SnackbarDuration.Short,
+                )
             }
+            delay(SnackBarDuration)
+            job.cancel()
         }
     }
 
@@ -84,7 +82,7 @@ internal fun MainScreen(
             )
         },
         snackbarHost = {
-            SnackbarHost(hostState = snackBarstate)
+            SnackbarHost(hostState = snackBarState)
         },
         containerColor = White,
     ) { innerPadding ->
