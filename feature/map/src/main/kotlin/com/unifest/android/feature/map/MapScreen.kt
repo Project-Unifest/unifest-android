da28a8016201d7f7508aa8be89ff425035cccf9e
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-07 22:39:17 +0900

    [style] 부스 아이템 description 텍스트 높이 항상 두줄의 높이를 채우도록

--- feature/map/src/main/kotlin/com/unifest/android/feature/map/MapScreen.kt
+++ feature/map/src/main/kotlin/com/unifest/android/feature/map/MapScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -35,6 +36,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,6 +44,7 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -438,6 +441,12 @@ fun BoothItem(
     onAction: (MapUiAction) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val density = LocalDensity.current
+    val textStyle = Content2
+    val textHeight = remember(textStyle) {
+        with(density) { Content2.fontSize.toDp() * 2 }
+    }
+
     Card(
         modifier = modifier.clickable {
             onAction(MapUiAction.OnBoothItemClick(boothInfo.id))
@@ -471,6 +480,7 @@ fun BoothItem(
                         maxLines = 2,
                         overflow = TextOverflow.Ellipsis,
                         style = Content2,
+                        modifier = Modifier.heightIn(min = textHeight),
                     )
                     Spacer(modifier = Modifier.height(3.dp))
                     Row(
