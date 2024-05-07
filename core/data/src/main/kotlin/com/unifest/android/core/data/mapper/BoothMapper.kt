134bcef867b28a1720383090fcf5fcdfea5dde3a
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-07 17:32:10 +0900

    [fix] BoothDetail api 변경 사항 반영
    likes 파라미터 제거

--- core/data/src/main/kotlin/com/unifest/android/core/data/mapper/BoothMapper.kt
+++ core/data/src/main/kotlin/com/unifest/android/core/data/mapper/BoothMapper.kt
@@ -19,7 +19,6 @@ internal fun BoothDetail.toModel(): BoothDetailModel {
         latitude = latitude,
         longitude = longitude,
         menus = menus.map { it.toModel() },
-        likes = likes,
         isLiked = false,
     )
 }
