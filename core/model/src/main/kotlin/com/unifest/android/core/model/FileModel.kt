bc4bd81cc66ef59c9378ad13b9614ccd88121bab
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-08 18:22:00 +0900

    [chore] 사용하지 않는 파일 삭제

--- core/model/src/main/kotlin/com/unifest/android/core/model/FileModel.kt
+++
@@ -1,9 +0,0 @@
-package com.unifest.android.core.model
-
-import androidx.compose.runtime.Stable
-
-@Stable
-data class FileModel(
-    val imgUrl: String,
-    val imgName: String,
-)
--- core/model/src/main/kotlin/com/unifest/android/core/model/MenuCreateModel.kt
+++
@@ -1,7 +0,0 @@
-package com.unifest.android.core.model
-
-data class MenuCreateModel(
-    val name: String,
-    val price: Int,
-    val imgUrl: String,
-)
