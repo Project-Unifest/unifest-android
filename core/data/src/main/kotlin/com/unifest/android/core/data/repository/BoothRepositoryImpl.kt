18ca90dd3d579382719a4987a7cbaa849665facb
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-08 19:07:26 +0900

    [chore] code style check success

--- core/data/src/main/kotlin/com/unifest/android/core/data/repository/BoothRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/unifest/android/core/data/repository/BoothRepositoryImpl.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import com.unifest.android.core.common.getDeviceId
 import com.unifest.android.core.data.mapper.toModel
 import com.unifest.android.core.data.util.runSuspendCatching
-import com.unifest.android.core.model.BoothModel
 import com.unifest.android.core.network.request.LikeBoothRequest
 import com.unifest.android.core.network.service.UnifestService
 import dagger.hilt.android.qualifiers.ApplicationContext
