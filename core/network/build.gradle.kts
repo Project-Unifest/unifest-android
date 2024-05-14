ddbeda68826f2e7b8c0c08bc37254dcb21e1c119
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-14 17:11:25 +0900

    [feat] debug/release server base url 분리 준비

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,5 +1,9 @@
 @file:Suppress("INLINE_FROM_HIGHER_PLATFORM")
 
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.unifest.android.library)
     alias(libs.plugins.unifest.android.retrofit)
@@ -8,11 +12,25 @@ plugins {
     id("kotlinx-serialization")
 }
 
+val localPropertiesFile = rootProject.file("local.properties")
+val localProperties = Properties()
+localProperties.load(FileInputStream(localPropertiesFile))
+
 android {
     namespace = "com.unifest.android.core.network"
 
     buildFeatures {
         buildConfig = true
+    }
+
+    buildTypes {
+        getByName("debug") {
+            buildConfigField("String", "SERVER_BASE_URL", getServerBaseUrl("DEBUG_SERVER_BASE_URL"))
+        }
+
+        getByName("release") {
+            buildConfigField("String", "SERVER_BASE_URL", getServerBaseUrl("DEBUG_SERVER_BASE_URL"))
+        }
     }
 }
 
@@ -26,4 +44,8 @@ dependencies {
 
 secrets {
     defaultPropertiesFileName = "secrets.properties"
+}
+
+fun getServerBaseUrl(propertyKey: String): String {
+    return gradleLocalProperties(rootDir).getProperty(propertyKey)
 }
