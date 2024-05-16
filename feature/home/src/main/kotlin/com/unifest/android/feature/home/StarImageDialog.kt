5dc87d3aa240bf71536c96a0a99e483857b45286
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-05-17 04:09:16 +0900

    [refactor] 연산 최소화

--- feature/home/src/main/kotlin/com/unifest/android/feature/home/StarImageDialog.kt
+++ feature/home/src/main/kotlin/com/unifest/android/feature/home/StarImageDialog.kt
@@ -5,12 +5,12 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
-import androidx.compose.ui.window.DialogProperties
 import com.unifest.android.core.model.StarInfoModel
 import com.unifest.android.core.ui.component.LargeStarImage
 
@@ -22,18 +22,17 @@ fun StarImageDialog(
     modifier: Modifier = Modifier,
 ) {
     val configuration = LocalConfiguration.current
-    val screenWidth = configuration.screenWidthDp.dp
-    val screenHeight = configuration.screenHeightDp.dp
-    val dialogSize = min(screenWidth, screenHeight) - 84.dp
+    val dialogSize = remember(configuration) {
+        val screenWidth = configuration.screenWidthDp.dp
+        val screenHeight = configuration.screenHeightDp.dp
+        min(screenWidth, screenHeight) - 36.dp
+    }
 
     BasicAlertDialog(
         onDismissRequest = onDismissRequest,
         modifier = modifier
             .size(dialogSize)
             .clip(CircleShape),
-        properties = DialogProperties(
-            usePlatformDefaultWidth = false,
-        ),
     ) {
         LargeStarImage(
             imgUrl = starInfo.imgUrl,
