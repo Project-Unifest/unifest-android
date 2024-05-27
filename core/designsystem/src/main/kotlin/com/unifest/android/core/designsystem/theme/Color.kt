2355b488ea8d2de724af4a9dde0c21745d2a0141
Jeong Sang Hoon <wjdtkdgns234@naver.com>
2024-05-27 17:19:29 +0900

    [design] 다크모드 일부 색 지정

--- core/designsystem/src/main/kotlin/com/unifest/android/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/kotlin/com/unifest/android/core/designsystem/theme/Color.kt
@@ -2,12 +2,11 @@ package com.unifest.android.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Secondary = Color(0xFFD9D9D9)
+val OnSecondary = Color(0xFF979797)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Secondary_dark = Color(0xFFD9D9DC)
+val OnSecondary_dark = Color(0xFFB0B0B0)
+val Background = Color(0xFF17171A)
 
 val MainColor = Color(0xFFF5687E)
--- core/designsystem/src/main/kotlin/com/unifest/android/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/unifest/android/core/designsystem/theme/Theme.kt
@@ -5,26 +5,27 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = MainColor,
+    onPrimary = Color.White,
+    background = Background,
+    onBackground = Color.White,
+    secondary =  Secondary_dark,
+    onSecondary = OnSecondary_dark,
+    scrim = Color.Black,
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-
-//    // Other default colors to override
-//    background = Color(0xFFFFFBFE),
-//    surface = Color(0xFFFFFBFE),
-//    onPrimary = Color.White,
-//    onSecondary = Color.White,
-//    onTertiary = Color.White,
-//    onBackground = Color(0xFF1C1B1F),
-//    onSurface = Color(0xFF1C1B1F),
+    primary = MainColor,
+    onPrimary = Color.White,
+    background = Color.White,
+    onBackground = Color.Black,
+    secondary = Secondary,
+    onSecondary = OnSecondary,
+    scrim = Color.Black,
 )
 
 @Composable
