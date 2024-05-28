1a46ca11d8dfb9ea3ef7094555765f64dcdf38ea
Jeong Sang Hoon <wjdtkdgns234@naver.com>
2024-05-28 21:48:13 +0900

    [feat] 웨이팅 화면 UI 작성

--- feature/waiting/src/main/kotlin/com/unifest/android/feature/waiting/viewmodel/WaitingUiState.kt
+++ feature/waiting/src/main/kotlin/com/unifest/android/feature/waiting/viewmodel/WaitingUiState.kt
@@ -1,6 +1,10 @@
 package com.unifest.android.feature.waiting.viewmodel
 
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
 data class WaitingUiState (
     val isServerErrorDialogVisible: Boolean = false,
     val isNetworkErrorDialogVisible: Boolean = false,
+//    val waitingLists: ImmutableList<WaitingModel> = persistentListOf(),
 )
