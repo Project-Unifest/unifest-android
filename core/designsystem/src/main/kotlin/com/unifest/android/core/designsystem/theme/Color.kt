c4d62d6f4625d17275da5754b2f59e19b7ddef12
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-07-11 23:26:31 +0900

    [style] 라이트모드, 다크모드에서 사용되는 색상 정의

--- core/designsystem/src/main/kotlin/com/unifest/android/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/kotlin/com/unifest/android/core/designsystem/theme/Color.kt
@@ -2,6 +2,52 @@ package com.unifest.android.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
+val LightPrimary900 = Color(0xFF4D101E)
+val LightPrimary800 = Color(0xFFBA3146)
+val LightPrimary700 = Color(0xFFE73D58)
+val LightPrimary500 = Color(0xFFFF748A)
+val LightPrimary300 = Color(0xFFF895A5)
+val LightPrimary100 = Color(0xFFFBC3CB)
+val LightPrimary50 = Color(0xFFFFF0F3)
+
+val DarkPrimary900 = Color(0xFFF1D9DE)
+val DarkPrimary800 = Color(0xFFEFA7B1)
+val DarkPrimary700 = Color(0xFFEF8192)
+val DarkPrimary500 = Color(0xFFF55770)
+val DarkPrimary300 = Color(0xFFD22F49)
+val DarkPrimary100 = Color(0xFFA93043)
+val DarkPrimary50 = Color(0xFF3F0F1A)
+
+val LightGrey900 = Color(0xFF131316)
+val LightGrey800 = Color(0xFF2B2B30)
+val LightGrey700 = Color(0xFF45464A)
+val LightGrey600 = Color(0xFF727276)
+val LightGrey500 = Color(0xFF8D8D93)
+val LightGrey400 = Color(0xFFBABABF)
+val LightGrey300 = Color(0xFFD4D6DC)
+val LightGrey200 = Color(0xFFE3E4EA)
+val LightGrey100 = Color(0xFFF1F3F7)
+
+val DarkGrey900 = Color(0xFFECEFF5)
+val DarkGrey800 = Color(0xFFD1D3DC)
+val DarkGrey700 = Color(0xFFB6B8C1)
+val DarkGrey600 = Color(0xFF9697A2)
+val DarkGrey500 = Color(0xFF78787F)
+val DarkGrey400 = Color(0xFF5A5A60)
+val DarkGrey300 = Color(0xFF343438)
+val DarkGrey200 = Color(0xFF212126)
+val DarkGrey100 = Color(0xFF131316)
+
+val LightBlueGreen = Color(0xFF1FC0BA)
+val LightRed = Color(0xFFFF5858)
+val LightOrange = Color(0xFFFF8A1F)
+val LightGreen = Color(0xFF15D055)
+
+val DarkBlueGreen = Color(0xFF00A8A1)
+val DarkRed = Color(0xFFF03939)
+val DarkOrange = Color(0xFFE16E05)
+val DarkGreen = Color(0xFF02B540)
+
 val LightBackground = Color(0xFFFFFFFF)
 val LightBottomBar = Color(0xFFFFFFFF)
 val LightBottomBarIcon = Color(0xFF555555)
@@ -21,4 +67,3 @@ val DarkAccent2 = Color(0xFF36282D)
 val DarkError = Color(0xFFD32F2F)
 
 val MainColor = Color(0xFFF5687E)
-val SubColor = Color(0xFFF5687E)
