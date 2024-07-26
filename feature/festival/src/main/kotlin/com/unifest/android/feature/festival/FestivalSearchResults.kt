3bcedaad9f0d5f393aab85f67030f0f13f505e9b
Jeong Sang Hoon <pathfinder8851@gmail.com>
2024-07-26 17:55:56 +0900

    [chore] code style check success

--- feature/festival/src/main/kotlin/com/unifest/android/feature/festival/FestivalSearchResults.kt
+++ feature/festival/src/main/kotlin/com/unifest/android/feature/festival/FestivalSearchResults.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
