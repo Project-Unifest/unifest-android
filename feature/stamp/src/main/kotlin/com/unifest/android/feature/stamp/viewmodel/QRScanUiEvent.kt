eeaddb3b9ffe28744930ff6e238b7f4821c63037
jihun.lee <jihun.lee@medisolution.co.kr>
2024-09-11 16:07:24 +0900

    [style] QR 스캔 화면 UI 구성 및 사용할 클래스, 인터페이스 정의

---
+++ feature/stamp/src/main/kotlin/com/unifest/android/feature/stamp/viewmodel/QRScanUiEvent.kt
@@ -0,0 +1,25 @@
+package com.unifest.android.feature.stamp.viewmodel
+
+
+sealed interface QRScanEvent {
+    data object NavigateBack : QRScanEvent
+    data object ScanSuccess : QRScanEvent
+    data class ScanError(val errorType: QRErrorType) : QRScanEvent
+}
+
+data class QRScanException(
+    val errorType: QRErrorType?,
+) : Exception(errorType?.name)
+
+enum class QRErrorType {
+    ShowNotToday, UsedTicket, TicketNotFound;
+
+    companion object {
+        fun fromString(type: String?): QRErrorType? = when (type?.trim()?.uppercase()) {
+            "SHOW_NOT_TODAY" -> ShowNotToday
+            "USED_TICKET" -> UsedTicket
+            "TICKET_NOT_FOUND" -> TicketNotFound
+            else -> null
+        }
+    }
+}
