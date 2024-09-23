c43870eb1e4b5fd8c99fd151edbd566eae5313b9
easyhooon <mraz3068@gmail.com>
2024-09-24 02:18:51 +0900

    [feat] FCM 주제 구독 관련 initializer 추가

---
+++ app/src/main/kotlin/com/unifest/android/initializer/FirebaseMessagingInitializer.kt
@@ -0,0 +1,23 @@
+package com.unifest.android.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.google.firebase.messaging.FirebaseMessaging
+import timber.log.Timber
+
+class FirebaseMessagingInitializer : Initializer<Unit> {
+    override fun create(context: Context) {
+        FirebaseMessaging.getInstance().subscribeToTopic("2")
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Timber.d("Subscribed to topic successfully")
+                } else {
+                    Timber.e("Failed to subscribe to topic")
+                }
+            }
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> {
+        return emptyList()
+    }
+}
