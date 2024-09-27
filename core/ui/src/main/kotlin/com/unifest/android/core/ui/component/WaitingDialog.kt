88fa08f079696bf2b5d07e8736462ce69507fa4b
easyhooon <mraz3068@gmail.com>
2024-09-27 23:37:15 +0900

    [chore] style check success

--- core/ui/src/main/kotlin/com/unifest/android/core/ui/component/WaitingDialog.kt
+++ core/ui/src/main/kotlin/com/unifest/android/core/ui/component/WaitingDialog.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
--- feature/festival/src/main/kotlin/com/unifest/android/feature/festival/FestivalBottomSheet.kt
+++ feature/festival/src/main/kotlin/com/unifest/android/feature/festival/FestivalBottomSheet.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberModalBottomSheetState
