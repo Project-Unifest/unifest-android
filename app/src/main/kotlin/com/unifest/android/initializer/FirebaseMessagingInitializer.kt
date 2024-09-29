209cc21afd084b6d47fea3413cf347f0b03b8689
easyhooon <mraz3068@gmail.com>
2024-09-29 21:05:55 +0900

    [feat] debug 빌드시 test topic 구독 로직 추가

--- app/src/main/kotlin/com/unifest/android/initializer/FirebaseMessagingInitializer.kt
+++ app/src/main/kotlin/com/unifest/android/initializer/FirebaseMessagingInitializer.kt
@@ -3,6 +3,7 @@ package com.unifest.android.initializer
 import android.content.Context
 import androidx.startup.Initializer
 import com.google.firebase.messaging.FirebaseMessaging
+import com.unifest.android.BuildConfig
 import timber.log.Timber
 
 class FirebaseMessagingInitializer : Initializer<Unit> {
@@ -15,6 +16,17 @@ class FirebaseMessagingInitializer : Initializer<Unit> {
                     Timber.e("Failed to subscribe to topic")
                 }
             }
+
+        if (BuildConfig.DEBUG) {
+            FirebaseMessaging.getInstance().subscribeToTopic("test")
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Timber.d("Subscribed to topic successfully")
+                    } else {
+                        Timber.e("Failed to subscribe to topic")
+                    }
+                }
+        }
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
