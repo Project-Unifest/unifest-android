5890f97ad9a3d6c60de025c306878934347d7d73
JeongSangHoon <wjdtkdgns234@naver.com>
2024-10-26 00:13:42 +0900

    [chore] code style check

--- feature/waiting/src/main/kotlin/com/unifest/android/feature/waiting/viewmodel/WaitingUiAction.kt
+++ feature/waiting/src/main/kotlin/com/unifest/android/feature/waiting/viewmodel/WaitingUiAction.kt
@@ -4,11 +4,11 @@ sealed interface WaitingUiAction {
     data class OnCancelWaitingClick(val waitingId: Long) : WaitingUiAction
     data class OnCancelNoShowWaitingClick(val waitingId: Long) : WaitingUiAction
     data class OnCheckBoothDetailClick(val boothId: Long) : WaitingUiAction
-//    data object OnPullToRefresh : WaitingUiAction
     data object OnWaitingCancelDialogCancelClick : WaitingUiAction
     data object OnWaitingCancelDialogConfirmClick : WaitingUiAction
     data object OnNoShowWaitingCancelDialogCancelClick : WaitingUiAction
     data object OnNoShowWaitingCancelDialogConfirmClick : WaitingUiAction
     data object OnLookForBoothClick : WaitingUiAction
     data object OnRefresh : WaitingUiAction
+    //    data object OnPullToRefresh : WaitingUiAction
 }
