9d72327b9cd040cb1c722f9e0de99103279c7c63
easyhooon <mraz3068@gmail.com>
2025-04-09 15:11:15 +0900

    [chore] 라이브러리 버전 최신화

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -55,7 +55,7 @@ androidComponents {
         val artifactsLoader = v.artifacts.getBuiltArtifactsLoader()
         v.instrumentationRunnerArguments.put(
             "targetAppId",
-            v.testedApks.map { artifactsLoader.load(it)?.applicationId }
+            v.testedApks.map { artifactsLoader.load(it)?.applicationId },
         )
     }
 }
