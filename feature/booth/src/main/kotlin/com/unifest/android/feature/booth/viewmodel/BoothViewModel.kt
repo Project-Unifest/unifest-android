4cba5d31e5053e31feb69cc96ccb2439387a6546
easyhooon <mraz3068@gmail.com>
2025-04-18 00:28:31 +0900

    [chore] 부스 상세 화면 코드 정리

--- feature/booth/src/main/kotlin/com/unifest/android/feature/booth/viewmodel/BoothViewModel.kt
+++ feature/booth/src/main/kotlin/com/unifest/android/feature/booth/viewmodel/BoothViewModel.kt
@@ -83,23 +83,11 @@ class BoothViewModel @Inject constructor(
                     }
                     val currentBoothId = _uiState.value.boothDetailInfo.id
                     val matchingBooth = _uiState.value.myWaitingList.find { it.boothId == currentBoothId }
-
                     when {
-                        matchingBooth?.status == "NOSHOW" -> {
-                            setNoShowDialogVisible(true)
-                        }
-
-                        matchingBooth != null -> {
-                            _uiEvent.send(BoothUiEvent.ShowSnackBar(UiText.StringResource(R.string.booth_waiting_already_exists)))
-                        }
-
-                        _uiState.value.myWaitingList.size >= 3 -> {
-                            _uiEvent.send(BoothUiEvent.ShowSnackBar(UiText.StringResource(R.string.booth_waiting_full)))
-                        }
-
-                        else -> {
-                            setPinCheckDialogVisible(true)
-                        }
+                        matchingBooth?.status == "NOSHOW" -> setNoShowDialogVisible(true)
+                        matchingBooth != null -> _uiEvent.send(BoothUiEvent.ShowSnackBar(UiText.StringResource(R.string.booth_waiting_already_exists)))
+                        _uiState.value.myWaitingList.size >= 3 -> _uiEvent.send(BoothUiEvent.ShowSnackBar(UiText.StringResource(R.string.booth_waiting_full)))
+                        else -> setPinCheckDialogVisible(true)
                     }
                 }
                 .onFailure { exception ->
