40e1d1489ff559a63e429839fc33f3ac117a7e0e
easyhooon <mraz3068@gmail.com>
2025-07-10 22:39:32 +0900

    [refactor] NetworkImage nullable 파라미터 not null로 변경 및 previewPlaceholder 추가
    LocalInspectionMode 직접 만들 필요 x

--- core/designsystem/src/main/kotlin/com/unifest/android/core/designsystem/component/NetworkImage.kt
+++ core/designsystem/src/main/kotlin/com/unifest/android/core/designsystem/component/NetworkImage.kt
@@ -1,12 +1,10 @@
 package com.unifest.android.core.designsystem.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.coil.CoilImage
@@ -19,39 +17,32 @@ import com.unifest.android.core.designsystem.theme.UnifestTheme
 
 @Composable
 fun NetworkImage(
-    imgUrl: String?,
-    contentDescription: String?,
+    imgUrl: String,
+    contentDescription: String,
     modifier: Modifier = Modifier,
     placeholder: Painter? = null,
     contentScale: ContentScale = ContentScale.Crop,
     onError: (Boolean) -> Unit = {},
 ) {
-    if (LocalInspectionMode.current) {
-        Image(
-            painter = painterResource(id = R.drawable.item_placeholder),
-            contentDescription = "Example Image Icon",
-            modifier = modifier,
-        )
-    } else {
-        CoilImage(
-            imageModel = { imgUrl },
-            modifier = modifier,
-            component = rememberImageComponent {
-                +PlaceholderPlugin.Loading(placeholder)
-                +PlaceholderPlugin.Failure(placeholder)
-            },
-            imageOptions = ImageOptions(
-                contentScale = contentScale,
-                alignment = Alignment.Center,
-                contentDescription = contentDescription,
-            ),
-            onImageStateChanged = { state ->
-                if (state is CoilImageState.Failure) {
-                    onError(true)
-                }
-            },
-        )
-    }
+    CoilImage(
+        imageModel = { imgUrl },
+        modifier = modifier,
+        component = rememberImageComponent {
+            +PlaceholderPlugin.Loading(placeholder)
+            +PlaceholderPlugin.Failure(placeholder)
+        },
+        imageOptions = ImageOptions(
+            contentScale = contentScale,
+            alignment = Alignment.Center,
+            contentDescription = contentDescription,
+        ),
+        onImageStateChanged = { state ->
+            if (state is CoilImageState.Failure) {
+                onError(true)
+            }
+        },
+        previewPlaceholder = painterResource(id = R.drawable.item_placeholder),
+    )
 }
 
 @ComponentPreview
