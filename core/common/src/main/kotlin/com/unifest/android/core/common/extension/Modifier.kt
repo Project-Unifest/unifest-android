2831b4dc97f7089dadee44ad226a4c7162b2f0ed
easyhooon <mraz3068@gmail.com>
2025-07-12 23:35:18 +0900

    [chore] 필요없는 @SuppressLint 구문 제거
    code style check success

--- core/common/src/main/kotlin/com/unifest/android/core/common/extension/Modifier.kt
+++ core/common/src/main/kotlin/com/unifest/android/core/common/extension/Modifier.kt
@@ -1,6 +1,5 @@
 package com.unifest.android.core.common.extension
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -13,7 +12,6 @@ import com.unifest.android.core.common.MultipleEventsCutter
 import com.unifest.android.core.common.get
 
 // https://stackoverflow.com/questions/66703448/how-to-disable-ripple-effect-when-clicking-in-jetpack-compose
-@SuppressLint("ModifierFactoryUnreferencedReceiver")
 inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
     clickable(
         indication = null,
@@ -23,7 +21,6 @@ inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier
     }
 }
 
-@Suppress("ModifierFactoryUnreferencedReceiver")
 fun Modifier.clickableSingle(
     enabled: Boolean = true,
     onClickLabel: String? = null,
--- feature/booth/src/main/kotlin/com/unifest/android/feature/booth/component/BoothDescription.kt
+++ feature/booth/src/main/kotlin/com/unifest/android/feature/booth/component/BoothDescription.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.LastBaseline
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
