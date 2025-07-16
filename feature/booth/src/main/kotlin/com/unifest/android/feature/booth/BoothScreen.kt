90ad8aa36c654a5d3f1ea75f5c10b0f14e62ec6e
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-07-17 01:32:54 +0900

    [feat] Booth 화면 텍스트 컴포넌트 구현

--- feature/booth/src/main/kotlin/com/unifest/android/feature/booth/BoothScreen.kt
+++ feature/booth/src/main/kotlin/com/unifest/android/feature/booth/BoothScreen.kt
@@ -1,22 +1,25 @@
 package com.unifest.android.feature.booth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -25,6 +28,8 @@ import com.unifest.android.core.common.ObserveAsEvents
 import com.unifest.android.core.designsystem.component.NetworkErrorDialog
 import com.unifest.android.core.designsystem.component.ServerErrorDialog
 import com.unifest.android.core.designsystem.theme.BoothTitle2
+import com.unifest.android.core.designsystem.theme.Content1
+import com.unifest.android.core.designsystem.theme.Content2
 import com.unifest.android.core.designsystem.theme.Content8
 import com.unifest.android.core.designsystem.theme.UnifestTheme
 import com.unifest.android.core.ui.DevicePreview
@@ -34,6 +39,7 @@ import com.unifest.android.feature.booth.viewmodel.BoothUiEvent
 import com.unifest.android.feature.booth.viewmodel.BoothUiState
 import com.unifest.android.feature.booth.viewmodel.BoothViewModel
 import com.unifest.android.feature.booth.viewmodel.ErrorType
+import com.unifest.android.core.designsystem.R as designR
 
 @Composable
 internal fun BoothRoute(
@@ -49,7 +55,7 @@ internal fun BoothRoute(
         }
     }
 
-    BoothContent(
+    BoothScreen(
         padding = padding,
         uiState = uiState,
         onAction = viewModel::onAction,
@@ -58,7 +64,7 @@ internal fun BoothRoute(
 }
 
 @Composable
-internal fun BoothContent(
+internal fun BoothScreen(
     padding: PaddingValues,
     uiState: BoothUiState,
     onAction: (BoothUiAction) -> Unit,
@@ -66,10 +72,14 @@ internal fun BoothContent(
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
             .padding(padding),
     ) {
-        Column { }
-
+        BoothContent(
+            uiState = uiState,
+            onAction = onAction,
+            modifier = Modifier.padding(horizontal = 20.dp),
+        )
 
         if (uiState.isServerErrorDialogVisible) {
             ServerErrorDialog(
@@ -85,6 +95,64 @@ internal fun BoothContent(
     }
 }
 
+@Composable
+internal fun BoothContent(
+    uiState: BoothUiState,
+    onAction: (BoothUiAction) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier.padding(top = 18.dp),
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+    ) {
+        item {
+            Text(
+                text = uiState.campusName,
+                color = MaterialTheme.colorScheme.onBackground,
+                style = Content8,
+            )
+        }
+        item {
+            Text(
+                text = stringResource(R.string.booth_list),
+                color = MaterialTheme.colorScheme.onBackground,
+                style = BoothTitle2,
+            )
+        }
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.total_booth_count, uiState.totalBoothCount),
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    style = Content2,
+                )
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(6.dp),
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(
+                            id = if (uiState.waitingAvailabilityChecked) designR.drawable.ic_check_waiting else designR.drawable.ic_check_waiting_unchecked,
+                        ),
+                        contentDescription = "check box icon",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.clickable {
+                            onAction(BoothUiAction.OnWaitingCheckBoxClick)
+                        },
+                    )
+                    Text(
+                        text = stringResource(id = R.string.waiting_availability),
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = Content1,
+                    )
+                }
+            }
+        }
+    }
+}
+
 @DevicePreview
 @Composable
 private fun StampScreenPreview(
@@ -92,7 +160,7 @@ private fun StampScreenPreview(
     boothUiState: BoothUiState,
 ) {
     UnifestTheme {
-        BoothContent(
+        BoothScreen(
             padding = PaddingValues(),
             uiState = boothUiState,
             onAction = {},
