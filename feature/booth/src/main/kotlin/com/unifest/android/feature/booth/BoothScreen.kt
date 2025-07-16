efb9f23e588ecedcc8af884d4b7407f0fc4fb464
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-07-16 22:31:58 +0900

    [feat] BoothTap Preview 생성

--- feature/booth/src/main/kotlin/com/unifest/android/feature/booth/BoothScreen.kt
+++ feature/booth/src/main/kotlin/com/unifest/android/feature/booth/BoothScreen.kt
@@ -3,16 +3,32 @@ package com.unifest.android.feature.booth
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.unifest.android.core.common.ObserveAsEvents
 import com.unifest.android.core.designsystem.component.NetworkErrorDialog
 import com.unifest.android.core.designsystem.component.ServerErrorDialog
+import com.unifest.android.core.designsystem.theme.BoothTitle2
+import com.unifest.android.core.designsystem.theme.Content8
+import com.unifest.android.core.designsystem.theme.UnifestTheme
+import com.unifest.android.core.ui.DevicePreview
+import com.unifest.android.feature.booth.preview.BoothPreviewParameterProvider
 import com.unifest.android.feature.booth.viewmodel.BoothUiAction
 import com.unifest.android.feature.booth.viewmodel.BoothUiEvent
 import com.unifest.android.feature.booth.viewmodel.BoothUiState
@@ -52,7 +68,7 @@ internal fun BoothContent(
             .fillMaxSize()
             .padding(padding),
     ) {
-        Column {  }
+        Column { }
 
 
         if (uiState.isServerErrorDialogVisible) {
@@ -68,3 +84,19 @@ internal fun BoothContent(
         }
     }
 }
+
+@DevicePreview
+@Composable
+private fun StampScreenPreview(
+    @PreviewParameter(BoothPreviewParameterProvider::class)
+    boothUiState: BoothUiState,
+) {
+    UnifestTheme {
+        BoothContent(
+            padding = PaddingValues(),
+            uiState = boothUiState,
+            onAction = {},
+        )
+    }
+}
+
