4a7c2298dca2719d4fba8bcfaeb91328f28ae023
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-08-07 14:09:34 +0900

    [refactor] 이미지 확대, 축소 로직 개선

--- feature/home/src/main/kotlin/com/unifest/android/feature/home/component/OriginalCardNews.kt
+++ feature/home/src/main/kotlin/com/unifest/android/feature/home/component/OriginalCardNews.kt
@@ -1,10 +1,9 @@
 package com.unifest.android.feature.home.component
 
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
@@ -14,7 +13,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalWindowInfo
 import coil.compose.AsyncImage
@@ -22,7 +24,6 @@ import com.unifest.android.core.designsystem.ComponentPreview
 import com.unifest.android.core.designsystem.theme.UnifestTheme
 import kotlin.math.absoluteValue
 
-// https://developer.android.com/develop/ui/compose/touch-input/pointer-input/multi-touch?hl=ko
 @Composable
 internal fun OriginalCardNews(
     modifier: Modifier = Modifier,
@@ -36,22 +37,52 @@ internal fun OriginalCardNews(
     var scale by remember { mutableFloatStateOf(1f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
 
-    val state = rememberTransformableState { zoomChange, offsetChange, _ ->
-        scale = (zoomChange * scale).coerceAtLeast(minimumValue = 1f)
-        val maxOffsetX = (((imageWidth * scale) - screenWidth) / 2)
-        val maxOffsetY = (((imageHeight * scale) - screenHeight) / 2).absoluteValue
-        offset = if (scale == 1f) {
-            Offset.Zero
-        } else {
-            Offset(
-                x = (offset.x + offsetChange.x * scale).coerceIn(-maxOffsetX, maxOffsetX),
-                y = (offset.y + offsetChange.y * scale).coerceIn(-maxOffsetY, maxOffsetY),
+    val scaledWidth by remember(imageWidth, scale) { derivedStateOf { imageWidth * scale } }
+    val scaledHeight by remember(imageHeight, scale) { derivedStateOf { imageHeight * scale } }
+    val imageRect by remember(
+        scaledWidth,
+        scaledHeight,
+        offset,
+    ) {
+        derivedStateOf {
+            Rect(
+                offset = Offset(
+                    x = ((screenWidth - scaledWidth) / 2f + offset.x).coerceAtLeast(0f),
+                    y = ((screenHeight - scaledHeight) / 2f + offset.y).coerceAtLeast(0f),
+                ),
+                size = Size(scaledWidth, scaledHeight),
             )
         }
     }
 
     Box(
-        modifier = modifier,
+        modifier = modifier
+            .pointerInput(Unit) {
+                detectTransformGestures { centroid, offestChange, zoomChange, _ ->
+                    if (!imageRect.contains(centroid)) return@detectTransformGestures
+                    // 줌 적용
+                    val newScale = (scale * zoomChange).coerceAtLeast(minimumValue = 1f)
+                    scale = newScale
+
+                    // 드래그 적용
+                    val maxOffsetX = ((scaledWidth - screenWidth) / 2f).absoluteValue
+                    val maxOffsetY = ((scaledHeight - screenHeight) / 2f).absoluteValue
+                    offset = if (scale == 1f) {
+                        Offset.Zero
+                    } else {
+                        Offset(
+                            x = (offset.x + offestChange.x).coerceIn(
+                                -maxOffsetX,
+                                maxOffsetX,
+                            ),
+                            y = (offset.y + offestChange.y).coerceIn(
+                                -maxOffsetY,
+                                maxOffsetY,
+                            ),
+                        )
+                    }
+                }
+            },
     ) {
         AsyncImage(
             modifier = Modifier
@@ -61,8 +92,6 @@ internal fun OriginalCardNews(
                     translationX = offset.x,
                     translationY = offset.y,
                 )
-                .transformable(state = state)
-                .fillMaxWidth()
                 .align(Alignment.Center)
                 .onGloballyPositioned { layoutCoordinates ->
                     imageWidth = layoutCoordinates.size.width
