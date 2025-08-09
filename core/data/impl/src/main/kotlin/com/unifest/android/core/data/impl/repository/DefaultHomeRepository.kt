f5b37d8e44049f34838750f606188a92776b2a76
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-08-09 17:46:14 +0900

    [feat] 홈 정보 가져오는 Repository 클래스 및 기능 구현

---
+++ core/data/impl/src/main/kotlin/com/unifest/android/core/data/impl/repository/DefaultHomeRepository.kt
@@ -0,0 +1,17 @@
+package com.unifest.android.core.data.impl.repository
+
+import com.unifest.android.core.data.api.repository.HomeRepository
+import com.unifest.android.core.data.mapper.toModel
+import com.unifest.android.core.data.util.runSuspendCatching
+import com.unifest.android.core.model.HomeInfoModel
+import com.unifest.android.core.network.service.UnifestService
+import javax.inject.Inject
+
+class DefaultHomeRepository @Inject constructor(
+    private val service: UnifestService
+): HomeRepository {
+    override suspend fun getHomeInfo(): Result<HomeInfoModel> = runSuspendCatching {
+        service.getHomeInfo().toModel()
+    }
+
+}
