3002445e1b1ad87dd2618d3edaa54c90109ac5c6
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-08-11 21:25:08 +0900

    [refactor] Retrofit Serializer 마이그레이션

--- core/network/src/main/kotlin/com/unifest/android/core/network/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/unifest/android/core/network/di/NetworkModule.kt
@@ -1,6 +1,5 @@
 package com.unifest.android.core.network.di
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.unifest.android.core.network.BuildConfig
 import dagger.Module
 import dagger.Provides
@@ -11,6 +10,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
