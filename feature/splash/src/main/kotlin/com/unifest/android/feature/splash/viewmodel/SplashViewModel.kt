1a14b25bf151518f2ba9e2295ddd12b30ad6ef09
easyhooon <mraz3068@gmail.com>
2025-09-06 20:30:14 +0900

    [feat] 앱 진입시 가천대 축제 관심 축제로 등록되도록 API 호출
    확성기를 통한 축제 알림을 받기 위해선 /interest 함수를 호출해야 함

--- feature/splash/src/main/kotlin/com/unifest/android/feature/splash/viewmodel/SplashViewModel.kt
+++ feature/splash/src/main/kotlin/com/unifest/android/feature/splash/viewmodel/SplashViewModel.kt
@@ -4,9 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.unifest.android.core.common.ErrorHandlerActions
 import com.unifest.android.core.common.handleException
+import com.unifest.android.core.data.api.repository.LikedFestivalRepository
 import com.unifest.android.core.data.api.repository.MessagingRepository
 import com.unifest.android.core.data.api.repository.OnboardingRepository
 import com.unifest.android.core.data.api.repository.RemoteConfigRepository
+import com.unifest.android.core.model.FestivalModel
+import com.unifest.android.feature.splash.BuildConfig
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
@@ -24,6 +27,7 @@ import javax.inject.Inject
 class SplashViewModel @Inject constructor(
     private val onboardingRepository: OnboardingRepository,
     private val messagingRepository: MessagingRepository,
+    private val likedFestivalRepository: LikedFestivalRepository,
     remoteConfigRepository: RemoteConfigRepository,
 ) : ViewModel(), ErrorHandlerActions {
     private val _uiState = MutableStateFlow(SplashUiState())
@@ -44,6 +48,8 @@ class SplashViewModel @Inject constructor(
 
     fun checkIntroCompletion() {
         viewModelScope.launch {
+            launch { registerLikedFestival() }
+
             if (onboardingRepository.checkIntroCompletion()) {
                 _uiEvent.send(SplashUiEvent.NavigateToMain)
             } else {
@@ -85,6 +91,17 @@ class SplashViewModel @Inject constructor(
             handleException(exception, this@SplashViewModel)
             false
         }
+    }
+
+    private suspend fun registerLikedFestival() {
+        val festivalId = if (BuildConfig.DEBUG) 1L else 15L
+        likedFestivalRepository.registerLikedFestival(FestivalModel(festivalId = festivalId))
+            .onSuccess {
+                Timber.d("festival registered: festivalId=$festivalId")
+            }
+            .onFailure { exception ->
+                Timber.e(exception)
+            }
     }
 
     private fun navigateToPlayStore() {
